f97bfba5baa2cc63fc087974351f9a9635f37cb6
yehyaa <yehyaa@umich.edu>
2022-04-30 16:19:49 -0400

    Initial Commit

---
+++ Travel Agent Admin System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Travel Agent Admin System
+//
+//  Created by Yehya Altamimi on 4/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
